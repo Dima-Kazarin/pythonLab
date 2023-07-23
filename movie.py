8e39d7eb7851d7d1139483c18c3d2e3ea19ed36f
Dima <dimakazarin66@gmail.com>
2023-07-23 11:38:17 +0300

    changes

--- movie.py
+++ movie.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 from datetime import datetime, timedelta
+from collections import Counter
 
 
 class MovieDataPreparation:
@@ -11,6 +12,8 @@ class MovieDataPreparation:
             'accept': 'application/json',
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTI3NGFmYTRlNTUyMjRjYzRlN2Q0NmNlMTNkOTZjOSIsInN1YiI6IjVkNmZhMWZmNzdjMDFmMDAxMDU5NzQ4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lbpgyXlOXwrbY0mUmP-zQpNAMCw_h-oaudAJB6Cn5c8'
         }
+        self.movies = self.get_sample_data()
+        self.genres = self.get_genre_data()
 
     def make_request(self, url):
         response = requests.get(url, headers=self.headers)
@@ -32,94 +35,65 @@ class MovieDataPreparation:
             movies.extend(self.get_movie_data(page))
         return movies
 
+    def get_all_data(self):
+        return self.movies
+
     def get_movies_with_indexes(self):
         movies = self.get_sample_data()
         return movies[3:20:4]
 
     def get_most_popular_title(self):
-        movies = self.get_sample_data()
-        movies.sort(key=lambda movie: movie.get('popularity', 0), reverse=True)
-        return movies[0].get('title', '')
+        return max(self.movies, key=lambda movie: movie.get('popularity', 0)).get('title', '')
 
     def get_movies_with_keywords(self, keywords):
-        movies = self.get_sample_data()
-        result = []
-        for movie in movies:
-            if any(keyword.lower() in movie.get('overview', '').lower() for keyword in keywords):
-                result.append(movie.get('title', ''))
-        return result
+        return [movie.get('title', '') for movie in self.movies if
+                any(keyword.lower() in movie.get('overview', '').lower() for keyword in keywords)]
 
     def get_unique_genres(self):
-        genres = self.get_genre_data()
-        unique_genres = set(genre['name'] for genre in genres)
-        return unique_genres
+        return set(genre['name'] for genre in self.genres)
 
     def remove_movies_by_genre(self, genre):
-        movies = self.get_sample_data()
-        genre_ids_to_remove = []
+        genre_ids_to_remove = [g['id'] for g in self.genres if g['name'].lower() == genre.lower()]
 
-        genres = self.get_genre_data()
-        for g in genres:
-            if g['name'].lower() == genre.lower():
-                genre_ids_to_remove.append(g['id'])
-
-        filtered_movies = [movie for movie in movies if
-                           not any(genre_id in genre_ids_to_remove for genre_id in movie.get('genre_ids', []))]
-
-        return filtered_movies
+        return [movie for movie in self.movies if
+                not any(genre_id in genre_ids_to_remove for genre_id in movie.get('genre_ids', []))]
 
     def get_popular_genre_counts(self):
-        movies = self.get_sample_data()
-        genre_counts = {}
-        for movie in movies:
+        genre_counts = Counter()
+        for movie in self.movies:
             genre_ids = movie.get('genre_ids', [])
-            for genre_id in genre_ids:
-                genre_counts[genre_id] = genre_counts.get(genre_id, 0) + 1
+            genre_counts.update(genre_ids)
 
-        genres = self.get_genre_data()
-        popular_genres = []
-        for genre in genres:
-            genre_id = genre.get('id')
-            genre_name = genre.get('name')
-            if genre_id in genre_counts:
-                popular_genres.append((genre_name, genre_counts[genre_id]))
+        popular_genres = [(genre['name'], count) for genre in self.genres for genre_id, count in genre_counts.items() if
+                          genre.get('id') == genre_id]
         popular_genres.sort(key=lambda x: x[1], reverse=True)
         return popular_genres
 
     def get_movies_grouped_by_genre(self):
-        movies = self.get_sample_data()
-        genres = self.get_genre_data()
-        genre_movies = {genre['id']: [] for genre in genres}
+        genre_movies = {genre['id']: [] for genre in self.genres}
 
-        for movie in movies:
+        for movie in self.movies:
             genre_ids = movie.get('genre_ids', [])
             for genre_id in genre_ids:
                 genre_movies[genre_id].append(movie.get('title', ''))
 
-        result = []
-        for genre_id, movies in genre_movies.items():
-            if len(movies) > 1:
-                result.append((genre_id, movies))
-        return result
+        return list(filter(lambda genre_info: len(genre_info[1]) > 1, genre_movies.items()))
 
     def modify_genre_ids(self):
-        movies = self.get_sample_data()
-        modified_movies = []
-
-        for movie in movies:
+        def modify_movie_genre_ids(movie):
             modified_movie = movie.copy()
             genre_ids = modified_movie.get('genre_ids')
             if genre_ids:
                 modified_movie['genre_ids'] = [22] + genre_ids[1:]
-            modified_movies.append(modified_movie)
+            return modified_movie
 
-        return movies, modified_movies
+        modified_movies = list(map(modify_movie_genre_ids, self.movies))
 
-    def get_movies_info(self):
-        movies = self.get_sample_data()
+        return self.movies, modified_movies
+
+    def get_movie_info(self):
         movies_info = []
-
-        for movie in movies:
+        for movie in self.movies:
             title = movie.get('title', '')
             popularity = round(movie.get('popularity', 0), 1)
             vote_average = int(movie.get('vote_average', 0))
@@ -134,7 +108,10 @@ class MovieDataPreparation:
                 'last_day_in_cinema': last_day_in_cinema.strftime('%Y-%m-%d')
             }
             movies_info.append(movie_info)
+        return movies_info
 
+    def get_movies_info(self):
+        movies_info = self.get_movie_info()
         movies_info.sort(key=lambda x: (x['vote_average'], x['popularity']), reverse=True)
         return movies_info
 
@@ -152,59 +129,49 @@ num_pages = 5
 
 movie_data = MovieDataPreparation(num_pages)
 
-sample_data = movie_data.get_sample_data()
+sample_data = movie_data.get_all_data()
 print('Sample data:')
-print(sample_data)
-print()
+print(sample_data, end='\n\n')
 
 movies_indexes = movie_data.get_movies_with_indexes()
 print('Movies with indexes 3 to 19 with a step of 4:')
-print(movies_indexes)
-print()
+print(movies_indexes, end='\n\n')
 
 most_popular_title = movie_data.get_most_popular_title()
 print('Most popular title:')
-print(most_popular_title)
-print()
+print(most_popular_title, end='\n\n')
 
 keywords = ['action', 'adventure']
 movies_with_keywords = movie_data.get_movies_with_keywords(keywords)
 print(f'Movies with keywords {keywords}:')
-print(movies_with_keywords)
-print()
+print(movies_with_keywords, end='\n\n')
 
 unique_genres = movie_data.get_unique_genres()
 print('Unique genres:')
-print(unique_genres)
-print()
+print(unique_genres, end='\n\n')
 
 genre_to_remove = 'Drama'
 filtered_movies = movie_data.remove_movies_by_genre(genre_to_remove)
 print(f'Movies after removing the genre "{genre_to_remove}":')
-print(filtered_movies)
-print()
+print(filtered_movies, end='\n\n')
 
 popular_genre_counts = movie_data.get_popular_genre_counts()
 print('Popular genres with their occurrence counts:')
-print(popular_genre_counts)
-print()
+print(popular_genre_counts, end='\n\n')
 
 movies_grouped_by_genre = movie_data.get_movies_grouped_by_genre()
 print('Movies grouped by genre:')
-print(movies_grouped_by_genre)
-print()
+print(movies_grouped_by_genre, end='\n\n')
 
 movies, modified_movies = movie_data.modify_genre_ids()
 print('Original movies:')
 print(movies)
 print('Modified movies with genre ID replaced:')
-print(modified_movies)
-print()
+print(modified_movies, end='\n\n')
 
 movies_info = movie_data.get_movies_info()
 print('Movies info:')
-print(movies_info)
-print()
+print(movies_info, end='\n\n')
 
 csv_filepath = 'movies_info.csv'
 movie_data.save_movies_info_to_csv(csv_filepath)
